98463569cb88fd4db24afb2f57121e507e5b41f8
Kamil Guszpit <kamil.guszpit@icloud.com>
2026-02-07 21:33:03 +0100

    ci(release): add npm publishing configuration

--- release.config.js
+++ release.config.js
@@ -4,11 +4,17 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
+        [
+            "@semantic-release/npm",
+            {
+                "pkgRoot": "packages/cli"
+            }
+        ],
         "@semantic-release/github",
         [
             "@semantic-release/git",
             {
-                "assets": ["CHANGELOG.md", "package.json"],
+                "assets": ["CHANGELOG.md", "packages/cli/package.json"],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ]
