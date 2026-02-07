da0973390d83cff1d8367b9edf5aec8e51b85dd3
Kamil Guszpit <kamil.guszpit@icloud.com>
2026-02-07 23:02:43 +0100

    fix: enable semantic-release for all commit types and fix version injection in CLI build

--- release.config.js
+++ release.config.js
@@ -4,8 +4,31 @@ module.exports = {
         { name: "develop", channel: "develop", prerelease: "beta" }
     ],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "conventionalcommits",
+                "releaseRules": [
+                    { "type": "feat", "release": "minor" },
+                    { "type": "fix", "release": "patch" },
+                    { "type": "perf", "release": "patch" },
+                    { "type": "refactor", "release": "patch" },
+                    { "type": "style", "release": "patch" },
+                    { "type": "docs", "release": "patch" },
+                    { "type": "build", "release": "patch" },
+                    { "type": "ci", "release": "patch" },
+                    { "type": "chore", "release": "patch" },
+                    { "type": "test", "release": "patch" },
+                    { "type": "revert", "release": "patch" }
+                ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                "preset": "conventionalcommits"
+            }
+        ],
         "@semantic-release/changelog",
         [
             "@semantic-release/npm",
